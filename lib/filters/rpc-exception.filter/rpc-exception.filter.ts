a8f72bcfe03d8cc33d436b0cf2aa82cb4fffa948
Oleksandr Bezrukov <sashabezrukovownmail@gmail.com>
2023-05-12 20:26:24 +0300

    Improved `RpcExceptionFilter`

--- lib/filters/rpc-exception.filter/rpc-exception.filter.ts
+++ lib/filters/rpc-exception.filter/rpc-exception.filter.ts
@@ -9,6 +9,16 @@ import { Observable, throwError } from 'rxjs';
 @Catch(RpcException)
 export class RpcExceptionFilter implements RpcFilter<RpcException> {
   catch(exception: RpcException, _host: ArgumentsHost): Observable<any> {
-    return throwError(() => exception.getError());
+    const rpcError = exception.getError();
+
+    const error =
+      typeof rpcError === 'string'
+        ? {
+            status: 'error',
+            message: rpcError,
+          }
+        : rpcError;
+
+    return throwError(() => error);
   }
 }
